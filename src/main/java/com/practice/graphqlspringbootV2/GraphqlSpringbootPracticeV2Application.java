4ec7e2901fe73e483b7eccc1a6fdf3e23f775a09
GerogeLeon <leongeroge@gmail.com>
2020-03-18 10:53:16 +0800

    init

---
+++ src/main/java/com/practice/graphqlspringbootV2/GraphqlSpringbootPracticeV2Application.java
@@ -0,0 +1,13 @@
+package com.practice.graphqlspringbootV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootPracticeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringbootPracticeV2Application.class, args);
+    }
+
+}
